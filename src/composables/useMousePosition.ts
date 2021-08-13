bdb13440b5cf24c5ef9f4800f8a21cf9bcd791ae
Martin Rojo <9095332+shmarts@users.noreply.github.com>
2021-08-13 16:40:05 +0100

    live coding

--- src/composables/useMousePosition.ts
+++ src/composables/useMousePosition.ts
@@ -1,1 +1,16 @@
-export default () => {}
+export default () => {
+  const x = ref(0)
+  const y = ref(0)
+
+  const handle = (e: MouseEvent) => {
+    x.value = e.pageX
+    y.value = e.pageY
+  }
+
+  window.addEventListener('mousemove', handle)
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', handle)
+  })
+
+  return { x, y }
+}
--- src/routes/After.vue
+++ src/routes/After.vue
@@ -1,3 +1,29 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import useMousePosition from '../composables/useMousePosition'
 
-<template></template>
+const count = ref(1)
+const previousCount = ref(0)
+
+const doubledCount = computed(() => count.value * 2)
+
+watch(count, (val, oldVal) => {
+  previousCount.value = oldVal
+})
+
+const { x, y } = useMousePosition()
+
+const increaseCount = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-lg font-medium">Count</h1>
+    <p class="mt-2 text-sm">Count is {{ count }}</p>
+    <p class="mt-2 text-sm">Count doubled is {{ doubledCount }}</p>
+    <p class="mt-2 text-sm">Count's previous value was {{ previousCount }}</p>
+    <button class="mt-6 w-full btn btn-primary" @click="increaseCount">Increase count</button>
+    <div class="mt-6">Mouse position {{ x }}, {{ y }}</div>
+  </div>
+</template>
